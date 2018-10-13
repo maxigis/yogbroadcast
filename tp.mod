cc8dd38956dede3a5dbbd3f4ab0ccdc16f9434e3
maxigis <maxig88@gmail.com>
2018-10-13 18:23:50 -0300

    Version entregada

---
+++ tp.mod
@@ -0,0 +1,63 @@
+# Conjuntos
+set EVENTOS;
+set JORNADA;
+
+# Parametros
+param CALIDAD {e in EVENTOS};
+param INICIO {e in EVENTOS};
+param FIN {e in EVENTOS};
+param HORA {j in JORNADA};
+
+# Variables
+var tr{e in EVENTOS} >= 0, binary;
+var y{e in EVENTOS, j in JORNADA} >= 0, binary;
+
+# Funcion objetivo
+maximize z: sum{e in EVENTOS} CALIDAD[e] * tr[e];
+
+# Restricciones
+s.t. inicio {e in EVENTOS, j in JORNADA}: HORA[j] >= INICIO[e] * y[e, j];
+s.t. fin {e in EVENTOS, j in JORNADA}: FIN[e] >= (HORA[j] + 1) * y[e, j];
+s.t. y_inf {e in EVENTOS}: sum{j in JORNADA} y[e, j] >= (FIN[e] - INICIO[e]) * tr[e];
+s.t. y_sup {e in EVENTOS}: sum{j in JORNADA} y[e, j] <= (FIN[e] - INICIO[e]) * tr[e];
+s.t. evento_hora {j in JORNADA}: sum{e in EVENTOS} y[e, j] <= 1;
+
+# Datos
+data;
+set EVENTOS 	:=  ARQ1 ARQ2 ATL1 ATL2 BAD1 BAD2;
+set JORNADA 	:=  1 2 3 4 5 6 7 8 9;
+
+param CALIDAD :=
+ARQ1					40
+ARQ2					20
+ATL1					60
+ATL2					50
+BAD1					30
+BAD2					20;
+
+param INICIO	:=
+ARQ1					1
+ARQ2					6
+ATL1					1
+ATL2					6
+BAD1					4
+BAD2					8;
+
+param FIN			:=
+ARQ1					4
+ARQ2					9
+ATL1					5
+ATL2					10
+BAD1					6
+BAD2					10;
+
+param HORA		:=
+1							1
+2							2
+3							3
+4							4
+5							5
+6							6
+7							7
+8							8
+9							9;
---
+++ tp.sol
@@ -0,0 +1,277 @@
+Problem:    tp
+Rows:       130
+Columns:    60 (60 integer, 60 binary)
+Non-zeros:  288
+Status:     INTEGER OPTIMAL
+Objective:  z = 120 (MAXimum)
+
+   No.   Row name        Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 z                         120                             
+     2 inicio[ARQ1,1]
+                                  -1            -1               
+     3 inicio[ARQ1,2]
+                                  -1            -2               
+     4 inicio[ARQ1,3]
+                                  -1            -3               
+     5 inicio[ARQ1,4]
+                                   0            -4               
+     6 inicio[ARQ1,5]
+                                   0            -5               
+     7 inicio[ARQ1,6]
+                                   0            -6               
+     8 inicio[ARQ1,7]
+                                   0            -7               
+     9 inicio[ARQ1,8]
+                                   0            -8               
+    10 inicio[ARQ1,9]
+                                   0            -9               
+    11 inicio[ARQ2,1]
+                                   0            -1               
+    12 inicio[ARQ2,2]
+                                   0            -2               
+    13 inicio[ARQ2,3]
+                                   0            -3               
+    14 inicio[ARQ2,4]
+                                   0            -4               
+    15 inicio[ARQ2,5]
+                                   0            -5               
+    16 inicio[ARQ2,6]
+                                   0            -6               
+    17 inicio[ARQ2,7]
+                                   0            -7               
+    18 inicio[ARQ2,8]
+                                   0            -8               
+    19 inicio[ARQ2,9]
+                                   0            -9               
+    20 inicio[ATL1,1]
+                                   0            -1               
+    21 inicio[ATL1,2]
+                                   0            -2               
+    22 inicio[ATL1,3]
+                                   0            -3               
+    23 inicio[ATL1,4]
+                                   0            -4               
+    24 inicio[ATL1,5]
+                                   0            -5               
+    25 inicio[ATL1,6]
+                                   0            -6               
+    26 inicio[ATL1,7]
+                                   0            -7               
+    27 inicio[ATL1,8]
+                                   0            -8               
+    28 inicio[ATL1,9]
+                                   0            -9               
+    29 inicio[ATL2,1]
+                                   0            -1               
+    30 inicio[ATL2,2]
+                                   0            -2               
+    31 inicio[ATL2,3]
+                                   0            -3               
+    32 inicio[ATL2,4]
+                                   0            -4               
+    33 inicio[ATL2,5]
+                                   0            -5               
+    34 inicio[ATL2,6]
+                                  -6            -6               
+    35 inicio[ATL2,7]
+                                  -6            -7               
+    36 inicio[ATL2,8]
+                                  -6            -8               
+    37 inicio[ATL2,9]
+                                  -6            -9               
+    38 inicio[BAD1,1]
+                                   0            -1               
+    39 inicio[BAD1,2]
+                                   0            -2               
+    40 inicio[BAD1,3]
+                                   0            -3               
+    41 inicio[BAD1,4]
+                                  -4            -4               
+    42 inicio[BAD1,5]
+                                  -4            -5               
+    43 inicio[BAD1,6]
+                                   0            -6               
+    44 inicio[BAD1,7]
+                                   0            -7               
+    45 inicio[BAD1,8]
+                                   0            -8               
+    46 inicio[BAD1,9]
+                                   0            -9               
+    47 inicio[BAD2,1]
+                                   0            -1               
+    48 inicio[BAD2,2]
+                                   0            -2               
+    49 inicio[BAD2,3]
+                                   0            -3               
+    50 inicio[BAD2,4]
+                                   0            -4               
+    51 inicio[BAD2,5]
+                                   0            -5               
+    52 inicio[BAD2,6]
+                                   0            -6               
+    53 inicio[BAD2,7]
+                                   0            -7               
+    54 inicio[BAD2,8]
+                                   0            -8               
+    55 inicio[BAD2,9]
+                                   0            -9               
+    56 fin[ARQ1,1]                -2            -4               
+    57 fin[ARQ1,2]                -3            -4               
+    58 fin[ARQ1,3]                -4            -4               
+    59 fin[ARQ1,4]                 0            -4               
+    60 fin[ARQ1,5]                 0            -4               
+    61 fin[ARQ1,6]                 0            -4               
+    62 fin[ARQ1,7]                 0            -4               
+    63 fin[ARQ1,8]                 0            -4               
+    64 fin[ARQ1,9]                 0            -4               
+    65 fin[ARQ2,1]                 0            -9               
+    66 fin[ARQ2,2]                 0            -9               
+    67 fin[ARQ2,3]                 0            -9               
+    68 fin[ARQ2,4]                 0            -9               
+    69 fin[ARQ2,5]                 0            -9               
+    70 fin[ARQ2,6]                 0            -9               
+    71 fin[ARQ2,7]                 0            -9               
+    72 fin[ARQ2,8]                 0            -9               
+    73 fin[ARQ2,9]                 0            -9               
+    74 fin[ATL1,1]                 0            -5               
+    75 fin[ATL1,2]                 0            -5               
+    76 fin[ATL1,3]                 0            -5               
+    77 fin[ATL1,4]                 0            -5               
+    78 fin[ATL1,5]                 0            -5               
+    79 fin[ATL1,6]                 0            -5               
+    80 fin[ATL1,7]                 0            -5               
+    81 fin[ATL1,8]                 0            -5               
+    82 fin[ATL1,9]                 0            -5               
+    83 fin[ATL2,1]                 0           -10               
+    84 fin[ATL2,2]                 0           -10               
+    85 fin[ATL2,3]                 0           -10               
+    86 fin[ATL2,4]                 0           -10               
+    87 fin[ATL2,5]                 0           -10               
+    88 fin[ATL2,6]                -7           -10               
+    89 fin[ATL2,7]                -8           -10               
+    90 fin[ATL2,8]                -9           -10               
+    91 fin[ATL2,9]               -10           -10               
+    92 fin[BAD1,1]                 0            -6               
+    93 fin[BAD1,2]                 0            -6               
+    94 fin[BAD1,3]                 0            -6               
+    95 fin[BAD1,4]                -5            -6               
+    96 fin[BAD1,5]                -6            -6               
+    97 fin[BAD1,6]                 0            -6               
+    98 fin[BAD1,7]                 0            -6               
+    99 fin[BAD1,8]                 0            -6               
+   100 fin[BAD1,9]                 0            -6               
+   101 fin[BAD2,1]                 0           -10               
+   102 fin[BAD2,2]                 0           -10               
+   103 fin[BAD2,3]                 0           -10               
+   104 fin[BAD2,4]                 0           -10               
+   105 fin[BAD2,5]                 0           -10               
+   106 fin[BAD2,6]                 0           -10               
+   107 fin[BAD2,7]                 0           -10               
+   108 fin[BAD2,8]                 0           -10               
+   109 fin[BAD2,9]                 0           -10               
+   110 y_inf[ARQ1]                 0            -0               
+   111 y_inf[ARQ2]                 0            -0               
+   112 y_inf[ATL1]                 0            -0               
+   113 y_inf[ATL2]                 0            -0               
+   114 y_inf[BAD1]                 0            -0               
+   115 y_inf[BAD2]                 0            -0               
+   116 y_sup[ARQ1]                 0                          -0 
+   117 y_sup[ARQ2]                 0                          -0 
+   118 y_sup[ATL1]                 0                          -0 
+   119 y_sup[ATL2]                 0                          -0 
+   120 y_sup[BAD1]                 0                          -0 
+   121 y_sup[BAD2]                 0                          -0 
+   122 evento_hora[1]
+                                   1                           1 
+   123 evento_hora[2]
+                                   1                           1 
+   124 evento_hora[3]
+                                   1                           1 
+   125 evento_hora[4]
+                                   1                           1 
+   126 evento_hora[5]
+                                   1                           1 
+   127 evento_hora[6]
+                                   1                           1 
+   128 evento_hora[7]
+                                   1                           1 
+   129 evento_hora[8]
+                                   1                           1 
+   130 evento_hora[9]
+                                   1                           1 
+
+   No. Column name       Activity     Lower bound   Upper bound
+------ ------------    ------------- ------------- -------------
+     1 tr[ARQ1]     *              1             0             1 
+     2 tr[ARQ2]     *              0             0             1 
+     3 tr[ATL1]     *              0             0             1 
+     4 tr[ATL2]     *              1             0             1 
+     5 tr[BAD1]     *              1             0             1 
+     6 tr[BAD2]     *              0             0             1 
+     7 y[ARQ1,1]    *              1             0             1 
+     8 y[ARQ1,2]    *              1             0             1 
+     9 y[ARQ1,3]    *              1             0             1 
+    10 y[ARQ1,4]    *              0             0             1 
+    11 y[ARQ1,5]    *              0             0             1 
+    12 y[ARQ1,6]    *              0             0             1 
+    13 y[ARQ1,7]    *              0             0             1 
+    14 y[ARQ1,8]    *              0             0             1 
+    15 y[ARQ1,9]    *              0             0             1 
+    16 y[ARQ2,1]    *              0             0             1 
+    17 y[ARQ2,2]    *              0             0             1 
+    18 y[ARQ2,3]    *              0             0             1 
+    19 y[ARQ2,4]    *              0             0             1 
+    20 y[ARQ2,5]    *              0             0             1 
+    21 y[ARQ2,6]    *              0             0             1 
+    22 y[ARQ2,7]    *              0             0             1 
+    23 y[ARQ2,8]    *              0             0             1 
+    24 y[ARQ2,9]    *              0             0             1 
+    25 y[ATL1,1]    *              0             0             1 
+    26 y[ATL1,2]    *              0             0             1 
+    27 y[ATL1,3]    *              0             0             1 
+    28 y[ATL1,4]    *              0             0             1 
+    29 y[ATL1,5]    *              0             0             1 
+    30 y[ATL1,6]    *              0             0             1 
+    31 y[ATL1,7]    *              0             0             1 
+    32 y[ATL1,8]    *              0             0             1 
+    33 y[ATL1,9]    *              0             0             1 
+    34 y[ATL2,1]    *              0             0             1 
+    35 y[ATL2,2]    *              0             0             1 
+    36 y[ATL2,3]    *              0             0             1 
+    37 y[ATL2,4]    *              0             0             1 
+    38 y[ATL2,5]    *              0             0             1 
+    39 y[ATL2,6]    *              1             0             1 
+    40 y[ATL2,7]    *              1             0             1 
+    41 y[ATL2,8]    *              1             0             1 
+    42 y[ATL2,9]    *              1             0             1 
+    43 y[BAD1,1]    *              0             0             1 
+    44 y[BAD1,2]    *              0             0             1 
+    45 y[BAD1,3]    *              0             0             1 
+    46 y[BAD1,4]    *              1             0             1 
+    47 y[BAD1,5]    *              1             0             1 
+    48 y[BAD1,6]    *              0             0             1 
+    49 y[BAD1,7]    *              0             0             1 
+    50 y[BAD1,8]    *              0             0             1 
+    51 y[BAD1,9]    *              0             0             1 
+    52 y[BAD2,1]    *              0             0             1 
+    53 y[BAD2,2]    *              0             0             1 
+    54 y[BAD2,3]    *              0             0             1 
+    55 y[BAD2,4]    *              0             0             1 
+    56 y[BAD2,5]    *              0             0             1 
+    57 y[BAD2,6]    *              0             0             1 
+    58 y[BAD2,7]    *              0             0             1 
+    59 y[BAD2,8]    *              0             0             1 
+    60 y[BAD2,9]    *              0             0             1 
+
+Integer feasibility conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
